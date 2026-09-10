2d42f1821afc28db89d6812b58121f9b7989f599
Ben Kallus <benjamin.p.kallus.gr@dartmouth.edu>
2026-09-10 04:38:25 -0400

    add all the compose files

---
+++ run_experiments.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -xmeuo pipefail
+
+docker compose -f lstar_rocq_gnutls_3_3_8.yml build
+docker compose -f lstar_rocq_gnutls_3_3_8.yml up
+docker wait lstar-rocq-ojo_lstar-rocq_1
+docker cp lstar-rocq-ojo_lstar-rocq_1:/tmp ./tmp
+mv ./tmp/*.dot ./lstar_rocq_gnutls_3_3_8.dot
+rm -rf ./tmp
+docker compose -f lstar_rocq_gnutls_3_3_8.yml down
+
+docker compose -f lstar_rocq_gnutls_3_3_12.yml build
+docker compose -f lstar_rocq_gnutls_3_3_12.yml up
+docker wait lstar-rocq-ojo_lstar-rocq_1
+docker cp lstar-rocq-ojo_lstar-rocq_1:/tmp ./tmp
+mv ./tmp/*.dot ./lstar_rocq_gnutls_3_3_12.dot
+rm -rf ./tmp
+docker compose -f lstar_rocq_gnutls_3_3_12.yml down
+
+docker compose -f statelearner_gnutls_3_3_8.yml build
+docker compose -f statelearner_gnutls_3_3_8.yml up
+docker wait lstar-rocq-ojo_statelearner_1
+docker cp lstar-rocq-ojo_statelearner_1:/app/statelearner/examples/openssl/output_server/learnedModel.dot ./statelearner_gnutls_3_3_8.dot
+docker compose -f statelearner_gnutls_3_3_8.yml down
+
+docker compose -f statelearner_gnutls_3_3_12.yml build
+docker compose -f statelearner_gnutls_3_3_12.yml up
+docker wait lstar-rocq-ojo_statelearner_1
+docker cp lstar-rocq-ojo_statelearner_1:/app/statelearner/examples/openssl/output_server/learnedModel.dot ./statelearner_gnutls_3_3_12.dot
+docker compose -f statelearner_gnutls_3_3_12.yml down
