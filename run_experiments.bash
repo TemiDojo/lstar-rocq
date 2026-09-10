a141e3dff6fdb24647b6261a777c235a195c61c3
Ben Kallus <benjamin.p.kallus.gr@dartmouth.edu>
2026-09-10 05:23:50 -0400

    add depends_on to compose files. give up on podman and just use docker. remove useless sleeps

--- run_experiments.bash
+++ run_experiments.bash
@@ -3,29 +3,29 @@
 set -xmeuo pipefail
 
 docker compose -f lstar_rocq_gnutls_3_3_8.yml build
-docker compose -f lstar_rocq_gnutls_3_3_8.yml up
-docker wait lstar-rocq_lstar-rocq_1
+docker compose -f lstar_rocq_gnutls_3_3_8.yml up -d
+docker wait lstar-rocq-lstar-rocq-1
 docker cp lstar-rocq_lstar-rocq_1:/tmp ./tmp
 mv ./tmp/*.dot ./lstar_rocq_gnutls_3_3_8.dot
 rm -rf ./tmp
 docker compose -f lstar_rocq_gnutls_3_3_8.yml down
 
 docker compose -f lstar_rocq_gnutls_3_3_12.yml build
-docker compose -f lstar_rocq_gnutls_3_3_12.yml up
-docker wait lstar-rocq_lstar-rocq_1
+docker compose -f lstar_rocq_gnutls_3_3_12.yml up -d
+docker wait lstar-rocq-lstar-rocq-1
 docker cp lstar-rocq_lstar-rocq_1:/tmp ./tmp
 mv ./tmp/*.dot ./lstar_rocq_gnutls_3_3_12.dot
 rm -rf ./tmp
 docker compose -f lstar_rocq_gnutls_3_3_12.yml down
 
 docker compose -f statelearner_gnutls_3_3_8.yml build
-docker compose -f statelearner_gnutls_3_3_8.yml up
-docker wait lstar-rocq_statelearner_1
+docker compose -f statelearner_gnutls_3_3_8.yml up -d
+docker wait lstar-rocq-statelearner-1
 docker cp lstar-rocq_statelearner_1:/app/statelearner/examples/openssl/output_server/learnedModel.dot ./statelearner_gnutls_3_3_8.dot
 docker compose -f statelearner_gnutls_3_3_8.yml down
 
 docker compose -f statelearner_gnutls_3_3_12.yml build
-docker compose -f statelearner_gnutls_3_3_12.yml up
-docker wait lstar-rocq_statelearner_1
+docker compose -f statelearner_gnutls_3_3_12.yml up -d
+docker wait lstar-rocq-statelearner-1
 docker cp lstar-rocq_statelearner_1:/app/statelearner/examples/openssl/output_server/learnedModel.dot ./statelearner_gnutls_3_3_12.dot
 docker compose -f statelearner_gnutls_3_3_12.yml down
