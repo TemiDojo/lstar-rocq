13f54e18a7b244f4f3a9bedad9229e6b0cf6ce36
Ben Kallus <benjamin.p.kallus.gr@dartmouth.edu>
2026-09-10 12:24:14 -0400

    _ => -

--- run_experiments.bash
+++ run_experiments.bash
@@ -5,7 +5,7 @@ set -xmeuo pipefail
 docker compose -f lstar_rocq_gnutls_3_3_8.yml build
 docker compose -f lstar_rocq_gnutls_3_3_8.yml up -d
 docker wait lstar-rocq-lstar-rocq-1
-docker cp lstar-rocq_lstar-rocq_1:/tmp ./tmp
+docker cp lstar-rocq-lstar-rocq-1:/tmp ./tmp
 mv ./tmp/*.dot ./lstar_rocq_gnutls_3_3_8.dot
 rm -rf ./tmp
 docker compose -f lstar_rocq_gnutls_3_3_8.yml down
@@ -13,7 +13,7 @@ docker compose -f lstar_rocq_gnutls_3_3_8.yml down
 docker compose -f lstar_rocq_gnutls_3_3_12.yml build
 docker compose -f lstar_rocq_gnutls_3_3_12.yml up -d
 docker wait lstar-rocq-lstar-rocq-1
-docker cp lstar-rocq_lstar-rocq_1:/tmp ./tmp
+docker cp lstar-rocq-lstar-rocq-1:/tmp ./tmp
 mv ./tmp/*.dot ./lstar_rocq_gnutls_3_3_12.dot
 rm -rf ./tmp
 docker compose -f lstar_rocq_gnutls_3_3_12.yml down
@@ -21,11 +21,11 @@ docker compose -f lstar_rocq_gnutls_3_3_12.yml down
 docker compose -f statelearner_gnutls_3_3_8.yml build
 docker compose -f statelearner_gnutls_3_3_8.yml up -d
 docker wait lstar-rocq-statelearner-1
-docker cp lstar-rocq_statelearner_1:/app/statelearner/examples/openssl/output_server/learnedModel.dot ./statelearner_gnutls_3_3_8.dot
+docker cp lstar-rocq-statelearner-1:/app/statelearner/examples/openssl/output_server/learnedModel.dot ./statelearner_gnutls_3_3_8.dot
 docker compose -f statelearner_gnutls_3_3_8.yml down
 
 docker compose -f statelearner_gnutls_3_3_12.yml build
 docker compose -f statelearner_gnutls_3_3_12.yml up -d
 docker wait lstar-rocq-statelearner-1
-docker cp lstar-rocq_statelearner_1:/app/statelearner/examples/openssl/output_server/learnedModel.dot ./statelearner_gnutls_3_3_12.dot
+docker cp lstar-rocq-statelearner-1:/app/statelearner/examples/openssl/output_server/learnedModel.dot ./statelearner_gnutls_3_3_12.dot
 docker compose -f statelearner_gnutls_3_3_12.yml down
